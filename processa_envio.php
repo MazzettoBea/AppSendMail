0568d0df50905577ed8e6fe0ca93d1c6f64855e9
Bianca Mazzetto Horacio <mazzebea@gmail.com>
2025-03-26 09:54:40 -0300

    at

---
+++ processa_envio.php
@@ -0,0 +1,10 @@
+<?php 
+require '../../appSend_mail/phpMailer/Exception.php';
+require '../../appSend_mail/phpMailer/OAuthTokenProvider.php'; 
+require '../../appSend_mail/phpMailer/OAuth.php';
+require '../../appSend_mail/phpMailer/PHPMailer.php';
+require '../../appSend_mail/phpMailer/SMTP.php';
+
+
+require"../../appSend_mail/processa_envio.php"; 
+?>
